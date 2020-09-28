998b0104caeccf6db51129e667302e00af120797
turnback <274114318@qq.com>
2020-09-28 23:40:46 +0800

    第一版

---
+++ dist/main.f3cd7d35.js
@@ -0,0 +1,225 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+parcelRequire = (function (modules, cache, entry, globalName) {
+  // Save the require from previous bundle to this closure if any
+  var previousRequire = typeof parcelRequire === 'function' && parcelRequire;
+  var nodeRequire = typeof require === 'function' && require;
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire = typeof parcelRequire === 'function' && parcelRequire;
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error('Cannot find module \'' + name + '\'');
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = cache[name] = new newRequire.Module(name);
+
+      modules[name][0].call(module.exports, localRequire, module, module.exports, this);
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x){
+      return newRequire(localRequire.resolve(x));
+    }
+
+    function resolve(x){
+      return modules[name][1][x] || x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [function (require, module) {
+      module.exports = exports;
+    }, {}];
+  };
+
+  var error;
+  for (var i = 0; i < entry.length; i++) {
+    try {
+      newRequire(entry[i]);
+    } catch (e) {
+      // Save first error but execute all entries
+      if (!error) {
+        error = e;
+      }
+    }
+  }
+
+  if (entry.length) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(entry[entry.length - 1]);
+
+    // CommonJS
+    if (typeof exports === "object" && typeof module !== "undefined") {
+      module.exports = mainExports;
+
+    // RequireJS
+    } else if (typeof define === "function" && define.amd) {
+     define(function () {
+       return mainExports;
+     });
+
+    // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+
+  // Override the current require with this new one
+  parcelRequire = newRequire;
+
+  if (error) {
+    // throw error from earlier, _after updating parcelRequire_
+    throw error;
+  }
+
+  return newRequire;
+})({"epB2":[function(require,module,exports) {
+var $siteList = $(".siteList");
+var $last = $siteList.find("li.last");
+var y = JSON.parse(localStorage.getItem("y"));
+var hashMap = y || [{
+  logo: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg",
+  url: "http://www.bilibili.com"
+}, {
+  logo: "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg",
+  url: "http://www.taobao.com"
+}];
+
+var simplifyUrl = function simplifyUrl(url) {
+  return url.replace("https://", "").replace("http://", "").replace("www.", "").replace(".com", "").replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+
+var render = function render() {
+  $siteList.find("li:not(.last)").remove(); //清空之前的
+
+  hashMap.forEach(function (node, index) {
+    var $li = $("\n          <li>\n              <div class=\"site\">\n                  <div class=\"logo\"><img src=\"".concat(node.logo, "\"></div>\n                  <div class=\"link\">").concat(simplifyUrl(node.url), "</div>\n                  <div class=\"close\">\n                    <svg class=\"icon\">\n                        <use xlink:href=\"#icon-close\"></use>\n                    </svg>\n                  </div>\n              </div>\n          </li>")).insertBefore($last);
+    $li.on("click", function () {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", function (e) {
+      e.stopPropagation();
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+
+render();
+$(".addButton").on("click", function () {
+  var url = window.prompt("请输入要添加的网址");
+
+  if (url.indexOf("http") !== 0 || url.indexOf("https") !== 0) {
+    url = "https://" + url;
+  }
+
+  var n = Math.ceil(Math.random() * 10);
+  var newLogo;
+
+  switch (n) {
+    case 1:
+      newLogo = "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2504392586,3499308163&fm=15&gp=0.jpg";
+      break;
+
+    case 2:
+      newLogo = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3351643673,1921012560&fm=15&gp=0.jpg";
+      break;
+
+    case 3:
+      newLogo = "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2382351343,687644440&fm=26&gp=0.jpg";
+      break;
+
+    case 4:
+      newLogo = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=88797792,1181454646&fm=15&gp=0.jpg";
+      break;
+
+    case 5:
+      newLogo = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2302215709,2875734388&fm=15&gp=0.jpg";
+      break;
+
+    case 6:
+      newLogo = "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg";
+      break;
+
+    case 7:
+      newLogo = "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1809374780,2828319758&fm=26&gp=0.jpg";
+      break;
+
+    case 8:
+      newLogo = "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=357409742,1965222523&fm=26&gp=0.jpg";
+      break;
+
+    case 9:
+      newLogo = "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1344739748,783129263&fm=26&gp=0.jpg";
+      break;
+
+    case 10:
+      newLogo = "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=459921552,931547644&fm=26&gp=0.jpg";
+      break;
+  }
+
+  hashMap.push({
+    logo: newLogo,
+    url: url
+  });
+  render();
+});
+
+window.onbeforeunload = function () {
+  var string = JSON.stringify(hashMap); //数组变字符串
+
+  localStorage.setItem("y", string);
+};
+
+$(".go").on("click", function () {
+  if ($(".choose").val() === "google") {
+    $(".searchForm").attr("action", "https://www.google.com/search");
+    $(".inputSearch").attr("name", "q");
+  }
+});
+},{}]},{},["epB2"], null)
+//# sourceMappingURL=main.f3cd7d35.js.map
---
+++ src/main.js
@@ -0,0 +1,115 @@
+const $siteList = $(".siteList");
+const $last = $siteList.find("li.last");
+const y = JSON.parse(localStorage.getItem("y"));
+const hashMap = y || [
+  {
+    logo:
+      "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg",
+    url: "http://www.bilibili.com",
+  },
+  {
+    logo:
+      "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg",
+    url: "http://www.taobao.com",
+  },
+];
+const simplifyUrl = (url) => {
+  return url
+    .replace("https://", "")
+    .replace("http://", "")
+    .replace("www.", "")
+    .replace(".com", "")
+    .replace(/\/.*/, ""); // 删除 / 开头的内容
+};
+const render = () => {
+  $siteList.find("li:not(.last)").remove(); //清空之前的
+  hashMap.forEach((node, index) => {
+    const $li = $(`
+          <li>
+              <div class="site">
+                  <div class="logo"><img src="${node.logo}"></div>
+                  <div class="link">${simplifyUrl(node.url)}</div>
+                  <div class="close">
+                    <svg class="icon">
+                        <use xlink:href="#icon-close"></use>
+                    </svg>
+                  </div>
+              </div>
+          </li>`).insertBefore($last);
+    $li.on("click", () => {
+      window.open(node.url);
+    });
+    $li.on("click", ".close", (e) => {
+      e.stopPropagation();
+      hashMap.splice(index, 1);
+      render();
+    });
+  });
+};
+render();
+$(".addButton").on("click", () => {
+  let url = window.prompt("请输入要添加的网址");
+  if (url.indexOf("http") !== 0 || url.indexOf("https") !== 0) {
+    url = "https://" + url;
+  }
+  let n = Math.ceil(Math.random() * 10);
+  let newLogo;
+  switch (n) {
+    case 1:
+      newLogo =
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2504392586,3499308163&fm=15&gp=0.jpg";
+      break;
+    case 2:
+      newLogo =
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3351643673,1921012560&fm=15&gp=0.jpg";
+      break;
+    case 3:
+      newLogo =
+        "https://ss1.bdstatic.com/70cFuXSh_Q1YnxGkpoWK1HF6hhy/it/u=2382351343,687644440&fm=26&gp=0.jpg";
+      break;
+    case 4:
+      newLogo =
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=88797792,1181454646&fm=15&gp=0.jpg";
+      break;
+    case 5:
+      newLogo =
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=2302215709,2875734388&fm=15&gp=0.jpg";
+      break;
+    case 6:
+      newLogo =
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=3500378932,2505168882&fm=26&gp=0.jpg";
+      break;
+    case 7:
+      newLogo =
+        "https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1809374780,2828319758&fm=26&gp=0.jpg";
+      break;
+    case 8:
+      newLogo =
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=357409742,1965222523&fm=26&gp=0.jpg";
+      break;
+    case 9:
+      newLogo =
+        "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1344739748,783129263&fm=26&gp=0.jpg";
+      break;
+    case 10:
+      newLogo =
+        "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=459921552,931547644&fm=26&gp=0.jpg";
+      break;
+  }
+
+  hashMap.push({
+    logo: newLogo,
+    url: url,
+  });
+  render();
+});
+window.onbeforeunload = () => {
+  const string = JSON.stringify(hashMap); //数组变字符串
+  localStorage.setItem("y", string);
+};
+$(".go").on("click", () => {
+  if ($(".choose").val() === "google") {
+    $(".searchForm").attr("action", "https://www.google.com/search");
+    $(".inputSearch").attr("name", "q");
+  }
+});
